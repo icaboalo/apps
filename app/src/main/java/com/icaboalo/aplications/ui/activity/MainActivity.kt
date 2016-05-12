14a85cebfe3da4c27bcd22cf73876741286c7d9f
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-05-12 11:41:20 -0500

    colors and app_bar

--- app/src/main/java/com/icaboalo/aplications/ui/activity/MainActivity.kt
+++ app/src/main/java/com/icaboalo/aplications/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.icaboalo.aplications.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import com.icaboalo.aplications.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+//    fun navigationViewOnClick() {
+//        mNavigationView!!.setNavigationItemSelectedListener { item ->
+//            var fragment: Fragment? = null
+//            when (item.itemId) {
+//                R.id.action_capture_list -> {
+//                    fragment = PurchasesFragment()
+//                }
+//            }
+//            replaceFragment(fragment!!)
+//            mDrawerLayout!!.closeDrawers()
+//            false
+//        }
+//    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
     }
 }
