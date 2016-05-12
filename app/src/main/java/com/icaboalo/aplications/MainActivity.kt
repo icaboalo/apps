79adcc22a3ddc0084ce84b55bcb7d4ad32c171e4
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-05-12 10:59:56 -0500

    kotlin configured

---
+++ app/src/main/java/com/icaboalo/aplications/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.icaboalo.aplications
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
