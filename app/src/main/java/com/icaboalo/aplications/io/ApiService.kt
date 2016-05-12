badad677328096cac85553df6bd2c4a2039578d9
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-05-12 11:34:30 -0500

    ApiClient & ApiService

--- app/src/main/java/com/icaboalo/aplications/io/ApiService.kt
+++ app/src/main/java/com/icaboalo/aplications/io/ApiService.kt
@@ -1,7 +1,14 @@
 package com.icaboalo.aplications.io
 
+import com.icaboalo.aplications.io.model.ResponseApiModel
+import retrofit2.Call
+import retrofit2.http.GET
+
 /**
  * Created by icaboalo on 12/05/16.
  */
 interface ApiService {
+
+    @GET("json")
+    fun getResponse(): Call<ResponseApiModel>
 }
